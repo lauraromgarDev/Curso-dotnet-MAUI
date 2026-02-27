c5fec80fcaa53dcaa671d86ef30c256fb8536e89
lauraromerogarcia <laura.romero@abptecnologica.com>
2026-02-27 09:19:27 +0100

    Escribimos un texto y lo traduce a numeros

--- Phoneword/Phoneword/MainPage.xaml.cs
+++ Phoneword/Phoneword/MainPage.xaml.cs
@@ -2,23 +2,55 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        string? translatedNumber;
 
         public MainPage()
         {
             InitializeComponent();
+
         }
 
-        private void OnCounterClicked(object? sender, EventArgs e)
+
+        private void TranslateButton_Clicked(object sender, EventArgs e)
         {
-            count++;
+            string enteredNumber = PhoneNumberText.Text;
+            translatedNumber = PhonewordTranslator.ToNumber(enteredNumber);
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
+            if (!string.IsNullOrEmpty(translatedNumber))
+            {
+                CallButton.IsEnabled = true;
+                CallButton.Text = "Call " + translatedNumber;
+            }
             else
-                CounterBtn.Text = $"Clicked {count} times";
+            {
+                CallButton.IsEnabled = false;
+                CallButton.Text = "Call";
+            }
+        }
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+        async void CallButton_Clicked(object sender, EventArgs e)
+        {
+            if (await this.DisplayAlertAsync(
+                "Dial a Number",
+                "Would you like to call " + translatedNumber + "?",
+                "Yes",
+                "No"))
+            {
+                try
+                {
+                    if (PhoneDialer.Default.IsSupported && !string.IsNullOrWhiteSpace(translatedNumber))
+                        PhoneDialer.Default.Open(translatedNumber);
+                }
+                catch (ArgumentNullException)
+                {
+                    await DisplayAlertAsync("Unable to dial", "Phone number was not valid.", "OK");
+                }
+                catch (Exception)
+                {
+                    // Other error has occurred.
+                    await DisplayAlertAsync("Unable to dial", "Phone dialing failed.", "OK");
+                }
+            }
         }
     }
 }
