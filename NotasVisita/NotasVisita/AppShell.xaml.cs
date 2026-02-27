21fee3f674c611d469663a9d761b4ad97b7aee97
lauraromerogarcia <laura.romero@abptecnologica.com>
2026-02-27 13:10:33 +0100

    Escribimos una nota, se guarda en un txt oculto y al volver a abrir la app se recupera el texto escrito

---
+++ NotasVisita/NotasVisita/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace NotasVisita
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ NotasVisita/NotasVisita/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace NotasVisita
+{
+    public partial class MainPage : ContentPage
+    {
+        string _filename = Path.Combine(FileSystem.AppDataDirectory, "notas.txt");
+        public MainPage()
+        {
+            InitializeComponent();
+            editor.Text = File.Exists(_filename) ? File.ReadAllText(_filename) : string.Empty;
+        }
+
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            File.WriteAllText(_filename, editor.Text);
+        }
+
+        private void btnBorrar_Clicked(object sender, EventArgs e)
+        {
+            if (File.Exists(_filename))
+            {
+                File.Delete(_filename);
+            }
+            editor.Text = string.Empty;
+        }
+    }
+}
