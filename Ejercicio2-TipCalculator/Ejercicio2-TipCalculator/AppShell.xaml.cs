48b6f77ad9a6cdade8eb5b3733221c466428f960
lauraromerogarcia <laura.romero@abptecnologica.com>
2026-03-02 14:05:15 +0100

    Ejercicio de calculadora de propinas con stackLayout

---
+++ Ejercicio2-TipCalculator/Ejercicio2-TipCalculator/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Ejercicio2_TipCalculator
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Ejercicio2-TipCalculator/Ejercicio2-TipCalculator/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Ejercicio2_TipCalculator
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
