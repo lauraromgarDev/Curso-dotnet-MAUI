af59abc0f3cc004c58cf100caf480bde17fa1692
lauraromerogarcia <laura.romero@abptecnologica.com>
2026-03-09 18:39:16 +0100

    El botón de añadir ahora funciona, nos redirige a la vista de crear un elemento

--- DoToo/DoToo/ViewModels/MainViewModel.cs
+++ DoToo/DoToo/ViewModels/MainViewModel.cs
@@ -1,24 +1,33 @@
-﻿using DoToo.Repositories;
+﻿using CommunityToolkit.Mvvm.Input;
+using DoToo.Repositories;
+using DoToo.Views;
 
 
 namespace DoToo.ViewModels
 {
-    public class MainViewModel : ViewModel
+    public partial class MainViewModel : ViewModel
     {
-        // Esto es un ejemplo de cómo inyectar el repositorio en el ViewModel.
-        // En una aplicación real, podrías usar un contenedor de inyección de
-        // dependencias para manejar esto.
+
         private readonly ITodoItemRepository repository;
 
-        // Constructor que recibe el repositorio como parámetro.
-        public MainViewModel(ITodoItemRepository repository)
+        //Este es un ejemplo de cómo inyectar el servicio de navegación en el ViewModel.
+        private readonly IServiceProvider services;
+
+        // Constructor que recibe el repositorio y el servicio de navegación.
+        public MainViewModel(ITodoItemRepository repository, IServiceProvider services)
         {
             this.repository = repository;
+            this.services = services;
             Task.Run(async () => await LoadDataAsync());
+
         }
 
         private async Task LoadDataAsync()
         {
         }
+
+        [RelayCommand]
+        public async Task AddItemAsync() =>
+               await Navigation.PushAsync(services.GetRequiredService<ItemView>());
     }
 }
