d11fea0a8527af29647c549e7464146c22da7544
lauraromerogarcia <laura.romero@abptecnologica.com>
2026-03-09 19:28:09 +0100

    Guardamos nuevos items y los mostramos

--- DoToo/DoToo/ViewModels/ItemViewModel.cs
+++ DoToo/DoToo/ViewModels/ItemViewModel.cs
@@ -1,14 +1,34 @@
-﻿using DoToo.Repositories;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using DoToo.Models;
+using DoToo.Repositories;
+using Microsoft.VisualBasic;
 
 namespace DoToo.ViewModels
 {
-    public class ItemViewModel : ViewModel
+    public partial class ItemViewModel : ViewModel
     {
         private readonly ITodoItemRepository repository;
+
+
+        [ObservableProperty]
+        TodoItem item;
 
         public ItemViewModel(ITodoItemRepository repository)
         {
             this.repository = repository;
+            Item = new TodoItem()
+            {
+                Due = DateTime.Now.AddDays(1)
+            };
+
+        }
+
+        [RelayCommand]
+        public async Task SaveAsync()
+        {
+            await repository.AddOrUpdateAsync(item);
+            await Navigation.PopAsync();
         }
     }
 }
--- DoToo/DoToo/ViewModels/MainViewModel.cs
+++ DoToo/DoToo/ViewModels/MainViewModel.cs
@@ -1,6 +1,9 @@
-﻿using CommunityToolkit.Mvvm.Input;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using DoToo.Models;
 using DoToo.Repositories;
 using DoToo.Views;
+using System.Collections.ObjectModel;
 
 
 namespace DoToo.ViewModels
@@ -16,6 +19,11 @@ namespace DoToo.ViewModels
         // Constructor que recibe el repositorio y el servicio de navegación.
         public MainViewModel(ITodoItemRepository repository, IServiceProvider services)
         {
+            repository.OnItemAdded += (sender, item) =>
+                items.Add(CreateTodoItemViewModel(item));
+            repository.OnItemUpdated += (sender, item) =>
+                Task.Run(async () => await LoadDataAsync());
+
             this.repository = repository;
             this.services = services;
             Task.Run(async () => await LoadDataAsync());
@@ -24,10 +32,28 @@ namespace DoToo.ViewModels
 
         private async Task LoadDataAsync()
         {
+            var items = await repository.GetItemsAsync();
+
+            var itemViewModels = items.Select(i =>
+                CreateTodoItemViewModel(i));
+            Items = new ObservableCollection<TodoItemViewModel>(itemViewModels);
+
+        }
+        private TodoItemViewModel CreateTodoItemViewModel(TodoItem item)
+        {
+            var itemViewModel = new TodoItemViewModel(item);
+            itemViewModel.ItemStatusChanged += ItemStatusChanged;
+            return itemViewModel;
+        }
+        private void ItemStatusChanged(object sender, EventArgs e)
+        {
         }
 
         [RelayCommand]
         public async Task AddItemAsync() =>
                await Navigation.PushAsync(services.GetRequiredService<ItemView>());
+
+        [ObservableProperty]
+        ObservableCollection<TodoItemViewModel> items;
     }
 }
